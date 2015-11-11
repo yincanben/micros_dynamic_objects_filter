e6479b31ae72c5998626c239892938d397f3cb1d
yincanben <yincanben@163.com>
2015-11-11 10:54:27 +0800

    Version 0.1

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ycb13/catkin_ws/src/Dynamic_object_filter
-# Build directory: /home/ycb13/catkin_ws/src/Dynamic_object_filter/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
--- build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/ycb13/catkin_ws/src/Dynamic_object_filter/build/gtest
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
